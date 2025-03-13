7407d22fb443fe24f8ca50f99580948404b9cdc5
sujan <sujankulal19@gmail.com>
2025-03-13 16:35:04 +0530

    Commit

---
+++ src/PrimeNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+// 0(root.n)
+public class PrimeNumber {
+    static boolean prime(int n){
+        if(n<= 1) return false;
+        for(int i = 2; i*i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(prime(n)){
+            System.out.println("Prime");
+        } else System.out.println("Not Primme");
+    }
+}
