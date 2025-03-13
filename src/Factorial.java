9d96cd3faa61c8f031186f28d1f384115593402b
sujan <sujankulal19@gmail.com>
2025-03-13 12:27:18 +0530

    commit

---
+++ src/Factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Factorial {
+    static int fact(int n){
+        int res =1;
+        if (n ==0 || n == 1) return 1;
+        for(int i=1;i<=n;i++){
+            res *= i;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+}
