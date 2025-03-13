263f6bd0e3743d7be5123bd7bedff7958869152b
sujan <sujankulal19@gmail.com>
2025-03-13 15:36:44 +0530

    commit

---
+++ src/HcfGcd.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+ public class HcfGcd {
+    //Brute Force O(min(a,b));
+   /* static int gcd(int a , int b){
+        int min=0;
+        if(a>b) {
+            min = a;
+        }else min = b;
+
+        for(int i = min ;i>=1;i--){
+            if(a % i ==0 && b%i==0){
+                return i;
+            }
+        }
+        return 1;
+    }
+    */
+
+// Optimal Solution O(Max(a,b))
+//    static int euclidGcd(int a, int b){
+//        while(a!=b){
+//            if(a>b){
+//                a = a-b;
+//            } else b = b-a;
+//        }
+//        return a;
+//    }
+
+
+//   Best O(log(min(a,b)))
+     static int euclidGcd(int a, int b){
+        while(a!= 0 && b!=0){
+            if(a>b){
+                a = a%b;
+            } else b = b%a;
+        }
+        if(a!=0) {
+            return a;
+         }
+        else return b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+     //   System.out.println(gcd(a,b));
+        System.out.println(euclidGcd(a,b));
+    }
+}
