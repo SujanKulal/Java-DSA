87e23452fce9710b5ec4c5477f373677bab4b6bc
sujan <sujankulal19@gmail.com>
2025-03-15 12:07:43 +0530

    Commit

--- src/PrimeNO_SieveofEratosthenes.java
+++ src/PrimeNO_SieveofEratosthenes.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class PrimeNO_SieveofEratosthenes {
-    static void sievePrime(int n){
+   /*  static void sievePrime(int n){
         boolean[] prime = new boolean[n+1];
         for(int i = 2; i<=n;i++){
             if(prime[i] == false){
@@ -12,6 +12,24 @@ public class PrimeNO_SieveofEratosthenes {
         }
         for(int i = 2; i<=n;i++){
             if(prime[i] == false){
+                System.out.println(i);
+            }
+        }
+    }
+    */
+
+    static void sievePrime(int n){
+        boolean[] prime = new boolean[n+1];
+        for(int i = 2; i*i<=n;i++){
+            if(!prime[i]){
+                for(int j = i*i;j<=n;j= j+i){
+                    prime[j] = true;
+                }
+            }
+        }
+
+        for(int i = 2;i<=n;i++){
+            if(!prime[i]){
                 System.out.println(i);
             }
         }
