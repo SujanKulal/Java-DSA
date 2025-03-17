1395f0bee57335d1df19f0e3863d37e309b52def
sujan <sujankulal19@gmail.com>
2025-03-17 16:08:22 +0530

    Commit

---
+++ src/Recursion/CountDigits.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class CountDigits {
+
+    static int count(int n){
+        if(n==0) return 0;
+        return count(n/10) + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count(886988));
+    }
+}
