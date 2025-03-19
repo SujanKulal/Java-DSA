e1cb5641c96462d7c4d704a1095879b12eb9f4dc
sujan <sujankulal19@gmail.com>
2025-03-19 12:55:50 +0530

    Added missing files

---
+++ src/Recursion/ReverseString.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class ReverseString {
+    static String reverseString(String s, String r, int i){
+        if(i<0) return r;
+        return reverseString(s,r+s.charAt(i),i-1);
+    }
+
+    public static void main(String[] args) {
+        String s = "Spiderman";
+        System.out.println(reverseString(s,"", s.length()-1));
+    }
+}
