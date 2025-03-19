8ae77f49476255bbeee25bbeb9e53b355c851b40
sujan <sujankulal19@gmail.com>
2025-03-19 12:36:06 +0530

    Commit

--- src/Recursion/CountDigits.java
+++ src/Recursion/CountDigits.java
@@ -4,10 +4,10 @@ public class CountDigits {
 
     static int count(int n){
         if(n==0) return 0;
-        return count(n/10) + 1;
+        return count(n/10)  +1;
     }
 
     public static void main(String[] args) {
-        System.out.println(count(886988));
+        System.out.println(count(886519988));
     }
 }
