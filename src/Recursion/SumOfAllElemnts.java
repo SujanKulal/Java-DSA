00cfba190e7d96fabb67a2731c05ba4609b13147
sujan <sujankulal19@gmail.com>
2025-03-20 14:48:09 +0530

    Commit

--- src/Recursion/SumOfAllElemnts.java
+++ src/Recursion/SumOfAllElemnts.java
@@ -10,7 +10,7 @@ public class SumOfAllElemnts {
     }
 
     public static void main(String[] args) {
-        int arr [] = {1,2,3,4,5};
+        int arr [] = {1,2,3,4,5,6};
         System.out.println(sum(arr,0));
     }
 }
