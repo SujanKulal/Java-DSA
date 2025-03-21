065a70efe729cc137dfda991c524f1fd12ee8c01
sujan <sujankulal19@gmail.com>
2025-03-21 22:56:48 +0530

    commit

---
+++ src/Recursion/JosephusProblem.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class JosephusProblem {
+    static int jos(int n, int k){
+        if(n ==1) return 0;
+        return (jos(n-1,k) + k) % n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(jos(7,3));
+    }
+}
