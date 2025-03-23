cf2de9cc59ac278cae6b778cb133ba96fe1e0b97
sujan <sujankulal19@gmail.com>
2025-03-23 15:13:55 +0530

    Commit

--- src/Recursion/BalancedParenthesis.java
+++
@@ -1,7 +0,0 @@
-package Recursion;
-
-public class BalancedParenthesis {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Recursion/GenerateBalancedParenthesis.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class BalancedParenthesis {
+    static void balPar(char[] ar, int i, int n, int o, int c){
+        if(i==ar.length){
+            System.out.println(ar);
+        }
+        if(o<n){
+            ar[i] = '(';
+            balPar(ar,i+1,n,o+1,c);
+        }
+        if(c<o){
+            ar[i] = ')';
+            balPar(ar,i+1,n,o,c+1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[] ar = new char [n*2];
+        balPar(ar,0,n,0,0);
+    }
+}
